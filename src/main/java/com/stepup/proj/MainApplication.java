ebae0310a7706fb4c24ee2d58c9c4047ba162967
Igor Solomeny <git.user.siv1983@inbox.ru>
2025-10-05 23:26:45 +0300

    доработка логики проекта

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,5 +3,6 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("helloWorld");
+        System.out.println("111");
     }
 }
