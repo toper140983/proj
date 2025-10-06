34a3081e724b5fe730c371917482cbd786dc32b4
Igor Solomeny <git.user.siv1983@inbox.ru>
2025-10-06 22:26:37 +0300

    добавлен "++++"

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,8 +3,8 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("helloWorld");
-        System.out.println("10011");
+        System.out.println("-----");
         System.out.println("2222");
-        System.out.println("333333");
+        System.out.println("+++");
     }
 }
